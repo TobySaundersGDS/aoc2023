fb73f70017e0c55a3894b9d86c411623672c3462
tobias.saunders <tobias.saunders@digital.cabinet-office.gov.uk>
2023-12-14 11:15:10 +0000

    day 2...

--- day2/main.py
+++ day2/main.py
@@ -1,27 +1,26 @@
 import tools
 
-possibles = [['red', 12], ['blue', 13], ['green', 14]]
-
 cubes_shown = tools.fetch_input('https://adventofcode.com/2023/day/2/input')
-#print(cubes_shown)
+
 cubes_list = tools.split_by_line(cubes_shown)
 valid_game_ids = []
+
 for game in cubes_list:
-    print(game)
+
     game_id = game[game.index(" ")+1: game.index(":")]
-    print(game_id)
+
     turns = game[game.index(":")+2:].split("; ")
-    print(turns)
+
     #is_valid = 1
     max_r = 0
     max_g = 0
     max_b = 0
     for turn in turns:
+
         handful = turn.split(", ")
-        print(handful)
 
         for blocks in handful:
-            print(blocks)
+
             num_in_hand = ''
             for letter in blocks:
                 if letter.isnumeric():
@@ -49,5 +48,5 @@ for game in cubes_list:
     valid_game_ids.append(max_b * max_g * max_r)
     #if is_valid == 1:
     #    valid_game_ids.append(int(game_id))
-    #            #print(game_id)
+
 print(sum(valid_game_ids))
